47d050d38f8bd263b01b6b5ab66b66d0c0fc445b
FancyNoemie <noemie.carrier@hotmail.com>
2023-01-16 13:25:08 -0500

    the balls work!

--- Exercice 1/js/script.js
+++ Exercice 1/js/script.js
@@ -9,11 +9,13 @@ author, and this description to match your project!
 "use strict";
 let xPosition;
 let yPosition;
-let y = 250;
+let i = 250;
 let border = 20;
 let wPlayer = 70;
 let ballY = 250;
 let ballX = 250;
+let moveX =0;
+let moveY = -1;
 
 function preload() {
 }
@@ -27,22 +29,45 @@ function setup() {
 function draw() {
     createCanvas(500,500);
     background(0,230,30);
-    rect(border, y-(wPlayer/2), border, wPlayer); //PLAYER
-    print(y);
+    ////////////////////////////////////PLAYERS ANIMATION///////////////////////////////////////////////
+    rect(border, i-(wPlayer/2), border, wPlayer); //PLAYER
+    
     if (keyIsDown(UP_ARROW)||keyIsDown(87)) {
-        y -= 5;
+        i -= 5;
       }
     if (keyIsDown(DOWN_ARROW)||keyIsDown(83)) {
-        y += 5;
+        i += 5;
       }
-    if (y<=0+(wPlayer/2)){
-        y=0+(wPlayer/2)
+    if (i<=0+(wPlayer/2)){
+        i=0+(wPlayer/2)
     }
-    if (y>=width-(wPlayer/2)){
-        y=width-(wPlayer/2)
+    if (i>=width-(wPlayer/2)){
+        i=width-(wPlayer/2)
     }
-    rect(width-border-border , y-(wPlayer/2), border , wPlayer); //OPPONENT
-    circle(ballY,ballX, 20);
+    rect(width-border-border , i-(wPlayer/2), border , wPlayer); //OPPONENT
+    //////////////////////////////////////BALL ANIMATION//////////////////////////////////////////////////////
+    circle(ballX,ballY, 20);
+    
+    ballX = ballX + moveX;
+    ballY = ballY + moveY;
+    print(ballY);
+    if (ballY < 0){
+        moveY = 2;
+        
+    }
+    if (ballY > height){
+        moveY = -2;
+        print("hey");
+    }
+    print(ballY);
+    if (ballY < 0){
+        moveY = 2;
+        
+    }
+    if (ballY > height){
+        moveY = -2;
+        print("hey");
+    }swsssw
 }
 
 function keyIsDown (){
