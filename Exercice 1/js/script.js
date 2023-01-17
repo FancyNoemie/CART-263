2de27c5f78fb7ef29177238867821efe14820d68
FancyNoemie <noemie.carrier@hotmail.com>
2023-01-17 16:10:59 -0500

    Add project 1

--- Exercice 1/js/script.js
+++ Exercice 1/js/script.js
@@ -6,7 +6,7 @@ This is my attempt at the first exercice from CART263 at concordia
 the length of the paddles can be changed by changing the wPlayer variable OwO
 
 The code still needs minor details for the collision to be clean... it overlaps a bit.
-I used the text example from https://p5js.org/reference/#/p5/text to do the effect on the text at the end.
+And it might need a start button! XD
 */
 
 "use strict";
