e20894b73c5446d10b54767edb5bedc7726cf8eb
FancyNoemie <noemie.carrier@hotmail.com>
2023-02-07 09:36:56 -0500

    Fixing the colorpalette

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -20,25 +20,34 @@ Add sound effects
 "use strict";
 let lines = []; // declare an array
 var offset=0;
-var b = 0;
+var r;
+var g;
+var b;
+var r1;
+var g1;
+var b1;
 
 function preload() {
 }
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
     noStroke();
-    for(let x = 0; x < width; x+=10){
+    noiseDetail(2, 0.2);// original parameters (4,0.5)
+    for(let x = 0; x < width; x+=20){
         var y = height * noise(x);
         lines[x] = new Particle(x,y);
+        r = random(83,93);
+        g = random(19,29);
+        b = random(64,74);
+        lines[x].setColor(r,g,b);
     }
 }
 function draw() {
     background(33, 50, 56);
-    for(let i = 0; i< lines.length; i+= 10) {
-        noiseDetail(2, 0.2);// original parameters (4,0.5)
-        lines[i].drawRectangle();
+    for(let i = 0; i< lines.length; i+= 20) {
         lines[i].changeHeight(offset,i);
         lines[i].changeY(offset,i); 
+        lines[i].drawRectangle();
     }
     this.offset += 0.01;
 }
@@ -54,18 +63,23 @@ class Particle { //Class - The part of code that holds all the information to ma
         this.speed = 2;
         this.offset = 0;
         this.lineHeight = 0;
-        this.b = random(10,200);
+        this.colorRectangle;
     }
     drawRectangle(){ //Method - a function that belongs to a class. 
-        rect(this.x, this.y, 10, -this.lineHeight, 5)
-        fill(this.lineHeight,map(noise(offset),0,1,0,255),200);
-        this.offset += 0.001;
+        fill(this.colorRectangle);
+        rect(this.x, this.y, 19, -this.lineHeight, 5);
+    }
+    setColor(r,g,b){
+        //fill(this.lineHeight,map(noise(offset),0,1,0,255),b);
+        this.colorRectangle = color(r+this.offset, g, b);
+        this.offset += 0.1;
+       
     }
     changeHeight(offset,i){
-        this.lineHeight = 20+(noise((2+i/100)+offset))*300;
+        this.lineHeight = 50+(noise((2+i/100)+offset))*500;
         //lines[i].lineHeight = map(noise(i/100,offset),0,1,300,10); // why is it so different?????
     }
     changeY(offset,i){
-        this.y = map(noise(i/100,offset),0,1,250,400);
+        this.y = map(noise(i/100,offset),0,1,windowHeight/2,windowHeight);
     }
 }
