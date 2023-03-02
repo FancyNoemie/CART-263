65c2f7061da83f0afe76dce3ca88cf00a4f05c13
FancyNoemie <noemie.carrier@hotmail.com>
2023-03-02 10:44:05 -0500

    Spiral :D

--- Project 2/README.md
+++ Project 2/README.md
@@ -7,3 +7,23 @@ Ideally, the user will be able to navigate the different themes and subjects to 
 
 This will simply be a fun and interactive way to navigate the library, based on data from the projects submitted between 2005 and 2020.
 https://www.kaggle.com/datasets/kingburrito666/instructables-diy-all-projects?select=projects_circuits.csv
+
+
+Here is the chunk of code I used to detect errors in my .csv file. At first, the code would only break and not tell me which line of the csv file was wrong. I found out that some lines were empty, and the code could not return a string of text, because there was no text! This only points me to the problematic line, so I can do my data pre-processing.
+(in setup)
+for (var r = 0; r < circuits.getRowCount(); r++){ // Cycle through each row of the table
+    //points[r] = new DataPoint(circuits.getString(r,2)); 
+    try{//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+      console.log(circuits.getString(r,3));
+    }
+    catch(error){
+      console.error("There was an error of type ("+error+") at line "+r+" of the csv file.");
+      alert(r);
+    }
+  } 
+
+// The Prime Spiral (aka Ulam Spiral)
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/167-prime-spiral.html
+// https://youtu.be/a35KWEjRvc0
+
--- Project 2/js/script.js
+++ Project 2/js/script.js
@@ -1,17 +1,123 @@
-/**
 
-*/
+// State of spiral
+let x, y;
+let px, py; //(Previous x and y)
+let step = 1;
+let state = 0;
+let numSteps = 1;
+let turnCounter = 1;
 
-"use strict";
+// Scale / resolution of spiral
+let stepSize = 20;
+let totalSteps;
 
+// Data in .csv format
+let circuits;
+let cooking;
+let craft;
+let living;
+let outside;
+let workshop;
+let points = [];
 
 function preload() {
+  circuits = loadTable("assets/projects_circuits.csv", "csv", "header");
+  cooking = loadTable("assets/projects_cooking.csv", "csv", "header");
+  craft = loadTable("assets/projects_craft.csv", "csv", "header");
+  living = loadTable("assets/projects_living.csv", "csv", "header");
+  outside = loadTable("assets/projects_outside.csv", "csv", "header");
+  workshop = loadTable("assets/projects_workshop.csv", "csv", "header");
 }
+
+///////////////////////////////////SPIRAL/////////////////////////////////////////
+
 
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(1,30,30);
+
+  // set up spiral
+  const cols = width / stepSize;
+  const rows = height / stepSize;
+  totalSteps = cols * rows;
+  x = width / 2;
+  y = height / 2;
+  px = x;
+  py = y;
+
+  // Create particles (one for each step)
+  for(let i = 0; i < stepSize; i++){
+    points[i] = new Particle();
+  }
 }
 
-function draw() {
-
+// Function to test what subcategory the project is in
+function isPrime(value) {
+  if (value == 1) return false;
+  for (let i = 2; i <= sqrt(value); i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
+function draw(){
+  // If prime draw circle
+  if (isPrime(step)) {
+    for(let i = 0; i< stepSize; i+= 20) {
+    points[i].drawDatapoint(x,y,stepSize);
+    }
+  }
+
+  // Connect current to previous with a line
+  line(x, y, px, py);
+  px = x;
+  py = y;
+
+  // Move according to state
+  switch (state) {
+    case 0:
+      x += stepSize;
+      break;
+    case 1:
+      y -= stepSize;
+      break;
+    case 2:
+      x -= stepSize;
+      break;
+    case 3:
+      y += stepSize;
+      break;
+  }
+  
+  // Change state
+  if (step % numSteps == 0) {
+    state = (state + 1) % 4;
+    turnCounter++;
+    if (turnCounter % 2 == 0) {
+      numSteps++;
+    }
+  }
+  step++;
+
+  // Are we done?
+  if (step > totalSteps) {
+    noLoop();
+  }
+}
+
+/////////////////////////////////PARTICLES////////////////////////////////////////////////
+
+class Particle { //Class - The part of code that holds all the information to make an object, like a blue print. 
+  constructor() { //Constructor - A piece of code that runs when you make an instance, like Setup. 
+  //Every class needs a constructor. Its a good place to put all your variables. 
+    
+  }
+
+  drawDatapoint(x,y,stepSize){ //Method - a function that belongs to a class. 
+    fill(255);
+    stroke(255);
+    circle(x, y, stepSize * 0.5);
+  }
+}
